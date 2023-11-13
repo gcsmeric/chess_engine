c9414add2b4800f74aa74ecb36de8db57c344978
Geoffrey Meric <gcs.meric@gmail.com>
2023-11-12 21:37:17 -0500

    added user input move processing

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,54 @@ using std::cout;
 using std::endl;
 using std::cin;
 using namespace chess;
+using namespace uci;
+
+bool verifyInputMove(string str) {
+    // Check if the string has either 4 or 5 characters
+    if (str.size() != 4 && str.size() != 5) {
+        return false;
+    }
+
+    // Check conditions for the first 4 characters
+    if (islower(str[0]) && isdigit(str[1]) && islower(str[2]) && isdigit(str[3])) {
+        // Check specific range for the first and third characters
+        if (str[0] >= 'a' && str[0] <= 'h' && str[2] >= 'a' && str[2] <= 'h' &&
+            str[1] >= '1' && str[1] <= '8' && str[3] >= '1' && str[3] <= '8') {
+            // If the string has 5 characters, check the 5th character
+            if (str.size() == 5) {
+                char fifthChar = std::tolower(str[4]); // Convert to lowercase for case-insensitivity
+                if (fifthChar == 'n' || fifthChar == 'b' || fifthChar == 'r' || fifthChar == 'q') {
+                    return true;
+                }
+            } else {
+                // If the string has 4 characters, it's still considered valid
+                return true;
+            }
+        }
+    }
+
+    // If any condition is not met, return false
+    return false;
+}
 
 int main() {
     cout << "Launching chess program" << endl;
     Board board;
+    Move inputMove;
+    string inputMoveStr = "";
     while (true){
-        Move inputMove;
-        string inputMoveStr = "";
-        while (inputMoveStr == "") {
+        while (true) {
             cout << " Input your move: " << endl;
             cin >> inputMoveStr;
-            cout << inputMoveStr << endl;
+            if (verifyInputMove(inputMoveStr)) {
+                cout << "Move Accepted - Computing Response" << endl;
+                break;
+            }
         }
+        inputMove = uci::uciToMove(board, inputMoveStr);
+        board.makeMove(inputMove);
+        
+
 
     }
     
